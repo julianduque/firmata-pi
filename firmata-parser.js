d731b8688c13bb05b8706909c67af7f3ef39d34a
Francis Gulotta <wizard@roborooter.com>
2013-09-05 16:59:37 -0400

    Complete basic firmata callbacks, implemented in arduino firmata.

--- firmata-parser.js
+++ firmata-parser.js
@@ -14,20 +14,24 @@ var FirmataParser = module.exports = function () {
 util.inherits(FirmataParser, events.EventEmitter);
 
 var msg = FirmataParser.msg = {
-  reportFirmware: 0x79,
   firmwareVersionMajor: 0,
   firmwareVersionMinor: 1,
   reportVersion: 0xF9,
   FIRMATA_VERSION_MAJOR: 2,
   FIRMATA_VERSION_MINOR: 3,
   digitalMessage: 0x90, // 144
+  reportAnalog: 0xC0, // 192
+  reportDigital: 0xD0, //208
   analogMessage: 0xE0, // 224
   setPinMode: 0xF4, // 244
   startSysex: 0xF0, // 240
-  endSysex: 0xF7 // 247
+  endSysex: 0xF7, // 247
+  systemReset: 0xFF, //255
+  stringData: 0x71, // SYSEX 113
+  reportFirmware: 0x79, // SYSEX 121
 };
 
-var midiCallbacks = {};
+var callbacks = {};
 
 FirmataParser.prototype.write = function (data) {
   this.parser.write(data);
@@ -35,32 +39,54 @@ FirmataParser.prototype.write = function (data) {
 
 FirmataParser.prototype.bindEvents = function (data) {
   this.parser.on('midi', function (cmd, channel, data) {
-    if (midiCallbacks[cmd]) {
-      midiCallbacks[cmd].call(this, channel, data);
+    if (callbacks[cmd]) {
+      callbacks[cmd].call(this, channel, data);
     }
   }.bind(this));
 
   this.parser.on('sysex', function (cmd, data) {
-
+    if (callbacks[cmd]) {
+      callbacks[cmd].call(this, data);
+    }
   }.bind(this));
 };
 
-// callbacks
-
-midiCallbacks[msg.reportVersion] = function () {
+// Midi callbacks
+callbacks[msg.reportVersion] = function () {
   this.emit('reportVersion');
 };
 
-midiCallbacks[msg.analogMessage] = function (pin, data) {
+callbacks[msg.analogMessage] = function (pin, data) {
   this.emit('analogMessage', pin, Parser.decodeValue(data)[0]);
 };
 
-midiCallbacks[msg.digitalMessage] = function (pin, data) {
+callbacks[msg.digitalMessage] = function (pin, data) {
   this.emit('digitalMessage', pin, Parser.decodeValue(data)[0]);
 };
 
-midiCallbacks[msg.setPinMode] = function (meh, data) {
+callbacks[msg.setPinMode] = function (meh, data) {
   this.emit('setPinMode', data[0], data[1]);
+};
+
+callbacks[msg.systemReset] = function () {
+  this.emit('systemReset');
+};
+
+callbacks[msg.reportAnalog] = function (pin, data) {
+  this.emit('reportAnalog', pin, data[0]);
+};
+
+callbacks[msg.reportDigital] = function (pin, data) {
+  this.emit('reportDigital', pin, data[0]);
+};
+
+// SysEx callbacks
+callbacks[msg.reportFirmware] = function () {
+  this.emit('reportFirmware');
+};
+
+callbacks[msg.stringData] = function (data) {
+  this.emit('stringData', Parser.decodeString(data));
 };
 
 // static methods
--- test/firmata-parser.js
+++ test/firmata-parser.js
@@ -29,7 +29,7 @@ describe['.firmwareVersion'] = function (test) {
   test.done();
 };
 
-describe['midiCallbacks'] = {
+describe['callbacks'] = {
 
   setUp: function (cb) {
     this.firmata = new FirmataParser();
@@ -80,26 +80,52 @@ describe['midiCallbacks'] = {
     this.firmata.write([msg.setPinMode, pin, mode]);
     test.ok(spy.calledWith(pin, mode));
     test.done();
+  },
+
+  systemReset: function (test) {
+    var spy = sinon.spy();
+    this.firmata.on('systemReset', spy);
+    this.firmata.write([msg.systemReset]);
+    test.equal(spy.callCount, 1);
+    test.done();
+  },
+
+  reportAnalog: function (test) {
+    var spy = sinon.spy();
+    this.firmata.on('reportAnalog', spy);
+    var pin = 4;
+    var state = 1; //report
+    this.firmata.write([msg.reportAnalog + pin, state]);
+    test.ok(spy.calledWith(pin, state));
+    test.done();
+  },
+
+  reportDigital: function (test) {
+    var spy = sinon.spy();
+    this.firmata.on('reportDigital', spy);
+    var pin = 4;
+    var state = 1; //report
+    this.firmata.write([msg.reportDigital + pin, state]);
+    test.ok(spy.calledWith(pin, state));
+    test.done();
+  },
+
+  reportFirmware: function (test) {
+    var spy = sinon.spy();
+    this.firmata.on('reportFirmware', spy);
+    this.firmata.write([msg.startSysex, msg.reportFirmware, msg.endSysex]);
+    test.equal(spy.callCount, 1);
+    test.done();
+  },
+
+  stringData: function (test) {
+    var spy = sinon.spy();
+    this.firmata.on('stringData', spy);
+    var message = "H! how are you today?";
+    this.firmata.write([msg.startSysex, msg.stringData]);
+    this.firmata.write(Parser.encodeString(message));
+    this.firmata.write([msg.endSysex]);
+    test.ok(spy.calledWith(message));
+    test.done();
   }
-
-  // From Firmata.h on arduino firmata
-  // #define SYSTEM_RESET            0xFF // reset from MIDI
-
-  // extended command set using sysex (0-127/0x00-0x7F)
-  /* 0x00-0x0F reserved for user-defined commands */
-  // #define SERVO_CONFIG            0x70 // set max angle, minPulse, maxPulse, freq
-  // #define STRING_DATA             0x71 // a string message with 14-bits per char
-  // #define SHIFT_DATA              0x75 // a bitstream to/from a shift register
-  // #define I2C_REQUEST             0x76 // send an I2C read/write request
-  // #define I2C_REPLY               0x77 // a reply to an I2C read request
-  // #define I2C_CONFIG              0x78 // config I2C settings such as delay times and power pins
-  // #define EXTENDED_ANALOG         0x6F // analog write (PWM, Servo, etc) to any pin
-  // #define PIN_STATE_QUERY         0x6D // ask for a pin's current mode and value
-  // #define PIN_STATE_RESPONSE      0x6E // reply with pin's current mode and value
-  // #define CAPABILITY_QUERY        0x6B // ask for supported modes and resolution of all pins
-  // #define CAPABILITY_RESPONSE     0x6C // reply with supported modes and resolution
-  // #define ANALOG_MAPPING_QUERY    0x69 // ask for mapping of analog to pin numbers
-  // #define ANALOG_MAPPING_RESPONSE 0x6A // reply with mapping info
-  // #define SAMPLING_INTERVAL       0x7A // set the poll rate of the main loop
-
 };
